6a6c4aab2786f15e2b00ed89d52bc934db754efe
Laasya Ankem <sailaasyaankem@my.unt.edu>
2024-10-18 00:31:40 -0500

    The name of the configuration file is being requested, and it is opened up. All lines are stored in separate variables. The variable region holds name of initial region file, so use it to read in region1.csv.

--- commercial.cpp
+++ commercial.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 int main(){
+    string filename;
+    cout << "Enter the filename for the configuration: ";
+    std::getline(cin, filename);
 
-    cout << "this will be my commercial." << endl;
+    ifstream configFile(filename);
+
+   
+    if (configFile.is_open())
+    {
+
+        string region;
+        string time;
+        string refresh;
+
+        getline(configFile, region);
+       
+
+        getline(configFile, time);
+       
+
+        getline(configFile, refresh);
+
+       
+
+        configFile.close();
+
+} else {
+         cerr << "Could not read in file. ERROR." << endl;
+           return 1; 
+}
+
+
+
 
     return 0;
 }
