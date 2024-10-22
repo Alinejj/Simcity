e7e1b0c29af60ca4a9ab4547a5b85dc46ad53366
Laasya Ankem <sailaasyaankem@my.unt.edu>
2024-10-22 17:45:42 -0500

    Commerical file, will do future coding.

---
+++ commercial.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "commercial.h"
+
+using namespace std;
+
+int main(){
+    
+    cout<< "This is the commercial file." << endl;
+
+    return 0;
+}
---
+++ commercial.h
@@ -0,0 +1,20 @@
+#ifndef COMMERCIAL_H
+#define COMMERCIAL_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Commercial
+{
+	public:
+		// needs to be added
+	private:
+		// needs to be added
+
+
+
+};
+
+#endif
